cb0edead2ae3ee5838549aacb0bfc6ee57c2399e
Mario Espinosa <marioespinosafiguerez@gmail.com>
2020-11-09 17:42:59 +0100

    Average_NoChanges

--- average.cs
+++ average.cs
@@ -1,25 +1,20 @@
 using System;
-
-//The program will caulculate how many numbers of 1,2,3 or more digits are entered by the user since the user type "end"
-
 public class Average{
-	public static void Main(){
-		double oneDigit=0, twoDigits, threeDigits=0, moreThreeDigits;
-		string endWord;
-		Console.Write("Insert a mark: ");
-		endWord=Console.ReadLine();
-		while(endWord!="end")
-		{ 
-			twoDigits=Convert.ToDouble(endWord);
-			threeDigits++;  
-			oneDigit+=twoDigits;  
-			Console.Write("Insert a mark: ");
-			endWord=Console.ReadLine();
-		}
-		if(threeDigits!=0)
-		{
-			moreThreeDigits=oneDigit/twoDigits;  
-		Console.WriteLine("The average is {0}.",moreThreeDigits);
-		}
-	}
+public static void Main(){
+double a=0, b, c=0, d;
+string s;
+Console.Write("Insert a mark: ");
+s=Console.ReadLine();	// we read the mark as string to check the word "end"	 
+while(s!="end"){  // if the mark isn't an end then we pass it to double to work with it
+b=Convert.ToDouble(s);
+c++;   //counter of numbers
+a+=b;  // sum of all the notes
+Console.Write("Insert a mark: ");
+s=Console.ReadLine();
+}
+if(c!=0){
+	d=a/c;  //calculate the average
+Console.WriteLine("The average is {0}.",d);
+}
+}
 }
