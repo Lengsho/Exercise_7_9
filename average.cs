f3914e0804863af31b91d7089dc8fdbae7ca6f79
Mario Espinosa <marioespinosafiguerez@gmail.com>
2020-11-09 17:53:40 +0100

    Average_WithChanges

--- average.cs
+++ average.cs
@@ -1,20 +1,24 @@
 using System;
+
+//The program will caulculates the average of a set of marks, until the user types "end"
 public class Average{
-public static void Main(){
-double a=0, b, c=0, d;
-string s;
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();	// we read the mark as string to check the word "end"	 
-while(s!="end"){  // if the mark isn't an end then we pass it to double to work with it
-b=Convert.ToDouble(s);
-c++;   //counter of numbers
-a+=b;  // sum of all the notes
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();
-}
-if(c!=0){
-	d=a/c;  //calculate the average
-Console.WriteLine("The average is {0}.",d);
-}
-}
+	public static void Main(){
+		double number1=0, number2, number3=0, average;
+		string endWord;
+		Console.Write("Insert a mark: ");
+		endWord=Console.ReadLine();	 
+		while(s!="end")
+		{ 
+			number2=Convert.ToDouble(s);
+			number3++;  
+			number1+=number2;  
+			Console.Write("Insert a mark: ");
+			endWord=Console.ReadLine();
+		}
+		if(number3!=0)
+		{
+			average=number1/number3;  
+			Console.WriteLine("The average is {0}.",average);
+		}
+	}
 }
