bd5956a3471103bedc6cbe4b5e7d10b5f7c88a9b
Mario Espinosa <marioespinosafiguerez@gmail.com>
2020-11-09 18:01:32 +0100

    Digit_WithChanges

--- digits.cs
+++ digits.cs
@@ -1,34 +1,35 @@
 using System;
 
+//The program will calculate how many numbers of 1,2,3 or more digits are entered by the user until the user types "end"
+
 class digits
 {
  static void Main()
- {
-	 //counters of number of digits a is for one digit, b is for 2 digits, c is for 3 digits and d is for more than 3 digits
-     int a = 0, b = 0, c = 0, d = 0;
-      Console.Write("Insert a number: ");
-      // read the number as string to check the word "end"
-      string e = Console.ReadLine();
-     while(e!="end"){
-int input = Convert.ToInt32(e);
-       if(input != 0)
-{
-       if(input/10 == 0)
-           a ++;  // it only has one digit
-       else 
-       if (input/100 == 0)
-		b ++;  // the number has 2 digits
-        else
-        if (input/1000 == 0)
-        c ++;  // the number has 3 digits
-        else
-           d++;  // the number has more than 3 digits
+ {	
+    int oneDigit = 0, twoDigits = 0, threeDigits = 0, moreThreeDigits = 0;
+    Console.Write("Insert a number: ");
+    string endWord = Console.ReadLine();
+    while(endWord!="end")
+    {
+        int inputNumber = Convert.ToInt32(endWord);
+        if(inputNumber != 0)
+        {
+
+        if(inputNumber/10 == 0)
+            oneDigit ++;  
+            else 
+                if (inputNumber/100 == 0)
+                    twoDigits ++;  
+                    else
+                        if (inputNumber/1000 == 0)
+                            threeDigits ++;  
+                            else
+                                moreThreeDigits++;  
         }
-      e = Console.ReadLine();
-}
+      endWord = Console.ReadLine();
+    }
 
 Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", a, b, c, d); 
-
 
  }
 }
